bd83ed0029e2cd1f545dc172c04cd6f6b51c1b1b
Corey Gelbaugh <ncgelbaugh11@aol.com>
2018-07-24 00:10:28 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'esch-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'esch';
+}
